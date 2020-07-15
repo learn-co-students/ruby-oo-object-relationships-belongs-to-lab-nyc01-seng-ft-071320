68476181654f9cf60ac64b931d9e58640756cce5
loc.fitness1@gmail.com <loc.fitness1@gmail.com>
2020-07-15 16:33:43 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,11 @@
+class Author
+    attr_accessor :name 
+    
+    def initialize
+        @name = name
+    end 
+
+    def author
+        author.name
+    end 
+end 
---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+    attr_accessor :title, :author
+    
+    def initialize 
+        @title = title
+    end 
+
+    def post
+        post.title
+    end 
+
+end 
